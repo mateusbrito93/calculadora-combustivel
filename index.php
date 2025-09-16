3d1e9e7bf9751ae7f823b9e0110238db39894ece
Mateus Brito <106850437+mateusbrito93@users.noreply.github.com>
2025-09-16 17:19:36 -0300

    Adding icons ios & android

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
 
     <link rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⛽</text></svg>">
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="/icons/apple-touch-icon-512.png">
 </head>
 
 <body>
